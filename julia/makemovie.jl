7d0e5ee7301b5377ac10dc2d9ae1f0bcfa949bfa
山本凜 <karon0617@gmail.com>
2024-02-22 03:59:51 +0900

    updated

--- julia/makemovie.jl
+++ julia/makemovie.jl
@@ -1,4 +1,4 @@
-# makemovie dir で実行する.
+# makemovieディレクトリで実行する.
 run(`ffmpeg -r 15 -i untitled.%05d.ppm -vcodec libx264 -pix_fmt yuv420p -r 30 output.mp4`)
 run(`ffmpeg -i untitled.01000.ppm output.png`)
 run(`mv output.png /Users/2023_2gou/Desktop/r_yamamoto/Research/outputdir/imagedir/`)
